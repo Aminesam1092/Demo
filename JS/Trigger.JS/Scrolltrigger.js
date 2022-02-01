4654d3f3da7d822ebf0a74e3207ab7eff0e193dc
DOUBLALIAMINE <k020g1464@g.neec.ac.jp>
2022-02-01 09:28:52 +0900

    Recommit

---
+++ JS/Trigger.JS/Scrolltrigger.js
@@ -0,0 +1,94 @@
+gsap.to(".Topconteiner__Secondheadingtext",{
+    scrollTrigger: {
+        trigger: ".Topconteiner",
+        start: "top",
+        end: 'bottom center',
+    },
+    duration: 0.5,
+    opacity: 1,
+});
+
+gsap.to(".Descriptionitemconteiner",{
+    scrollTrigger: {
+        trigger: ".Descriptionitemconteiner",
+        start: "top center",
+        end: 'bottom center',
+    },
+    duration: 2,
+    opacity: 1,
+});
+
+gsap.to(".Descriptionitemsecondconteiner__Secondheadingtext",{
+    scrollTrigger: {
+        trigger: ".Descriptionitemsecondconteiner__Secondheadingtext",
+        start: "top center",
+        end: '+=2000',
+        scrub: true,
+        pin: true,
+    },
+    duration: 2,
+    opacity: 1,
+});
+gsap.to(".maguro",{
+    scrollTrigger: {
+        trigger: ".maguro",
+        start: "center center",
+        end: '+=1000',
+        scrub: true,
+        pin: true,
+    },
+});
+gsap.to(".ikura",{
+    scrollTrigger: {
+        trigger: ".ikura",
+        start: "center center",
+        end: '+=1000',
+        scrub: true,
+        pin: true,
+    },
+});
+gsap.to(".tamago",{
+    scrollTrigger: {
+        trigger: ".tamago",
+        start: "center center",
+        end: '+=1000',
+        scrub: true,
+        pin: true,
+    },
+});
+
+gsap.to(".Descriptionite__HeaddingSecondtext",{
+    scrollTrigger: {
+        trigger: ".Descriptionite__HeaddingSecondtext",
+        start: "top center",
+        end: '+=2000',
+        scrub: true,
+        pin: true,
+    },
+    duration: 2,
+    opacity: 1,
+},
+);
+
+gsap.to(".Secondnewline",{
+    scrollTrigger: {
+        trigger: ".Secondnewline",
+        start: "center center",
+        end: '+=1000',
+        scrub: true,
+        pin: true,
+    },
+    opacity: 1,
+});
+
+gsap.to(".Inductionconteiner",{
+    scrollTrigger: {
+        trigger: ".Inductionconteiner",
+        start: "center center",
+        end: '+=2000',
+        scrub: true,
+        pin: true,
+    },
+    duration: 2,
+    opacity: 1,
+});
